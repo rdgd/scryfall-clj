b1480e27f334d5b8f4244b2475d1566c6d183664
rdgd <ryan.is.gray@gmail.com>
2026-04-07 01:12:31 -0500

    add user agent and accept headers.

--- src/scryfall/http.clj
+++ src/scryfall/http.clj
@@ -5,7 +5,11 @@
   [url & [{:keys [method] :as params}]]
   (let [[http-fn params-key] (case method :post [http/post :form-params] [http/get :query-params])]
     (try
-      (-> (http-fn (str "https://api.scryfall.com/" url) {:as :json params-key params})
+      (-> (http-fn (str "https://api.scryfall.com/" url)
+                   {:as :json
+                    :headers {"User-Agent" "scryfall-clj"
+                              "Accept" "application/json"}
+                    params-key params})
           :body)
       (catch Exception e
         (throw (ex-info (str "Error occurred while calling " url (when params (str " with params " params)) e) (ex-data e)))))))
